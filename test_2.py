e24ba8613c2e002485aba4969c60dac1be626dfb
alex29z <alex29z@rambler.ru>
2021-02-04 00:16:30 +0300

    new

---
+++ test_2.py
@@ -0,0 +1,34 @@
+#!/usr/bin/pyton3
+
+def key_list(l):
+    m = list()
+    for i in l:
+        if i not in m :
+            m.append(i)
+    key = list()
+    key.append(m[0])
+    for i in range(1, len(m)) :
+        flag = True
+        for j in range(len(key)) :
+            if m[i].lower() == key[j].lower() :
+                flag = False
+                break
+        if flag :
+            key.append(m[i])
+    return key
+
+def dic(key, list) :
+    dict = {}
+    for i in range(len(key)):
+        count = 0
+        for j in range(len(list)):
+            if key[i].lower() == list[j].lower() :
+                count += 1
+        dict.update({key[i]:count})
+    return dict
+
+with open('1234.txt', 'r') as f:
+    l = f.read().split()
+print(l)
+print(key_list(l))
+print(max(dic(key_list(l), l), key = dic(key_list(l).get))
